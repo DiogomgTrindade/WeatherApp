9e5d04b7cf84b5a16d65db8a6483353024a740f5
DiogomgTrindade <diogovsky1904@gmail.com>
2024-11-05 13:44:36 +0000

    LoginPage, login authentication

---
+++ WeatherApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,38 @@
+using WeatherApp.API;
+
+namespace WeatherApp.Pages;
+
+public partial class LoginPage : ContentPage
+{
+	private readonly LoginService _loginService = new LoginService();
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		var username = UsernameEntry.Text;
+		var password = PasswordEntry.Text;
+
+		if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+		{
+			await DisplayAlert("Error", "Email or password cannot be empty.", "OK");
+			return;
+		}
+
+		var token = await _loginService.LoginAsync(username, password);
+		if (token != null)
+		{
+			await SecureStorage.SetAsync("auth_token", token);
+			_loginService.SetAuthorizationHeader(token);
+
+			//TODO: Alterar quando tiver a weather page
+			await DisplayAlert("Login", "You're Welcome", "OK");
+		}
+		else
+		{
+            await DisplayAlert("Erro", "Credenciais inválidas. Tente novamente.", "OK");
+        }
+    }
+}
---
+++ WeatherApp/Services/LoginService.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherApp.API
+{
+    public class LoginService
+    {
+
+        private readonly HttpClient _httpClient = new HttpClient();
+        private const string AuthBaseUrl = "https://globeairlinesdmgt.somee.com/api";
+
+        public async Task<string> LoginAsync(string username, string password)
+        {
+            var loginData = new { EmailAddress = username, Password = password };
+            var json = JsonConvert.SerializeObject(loginData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"{AuthBaseUrl}/auth/login", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var tokenObj = JsonConvert.DeserializeObject<dynamic>(jsonResponse);
+                var token = tokenObj.token.ToString();
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                return token;
+            }
+
+            return null;
+        }
+
+
+        public async Task<bool> RegisterAsync(string username, string password, string firstName, string lastName)
+        {
+            var registerData = new { EmailAdress = username, Password = password, FirstName = firstName, LastName = lastName };
+            var json = JsonConvert.SerializeObject(registerData);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync($"{AuthBaseUrl}/register", content);
+            return response.IsSuccessStatusCode;
+        }
+
+        public void SetAuthorizationHeader(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        }
+    }
+}
